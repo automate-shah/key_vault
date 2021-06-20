96688ad4c902f916e9d30c9848e528c001525cb4
Shehansha <86175381+automate-shah@users.noreply.github.com>
2021-06-20 20:17:11 +0530

    final commit

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "kv" {
 
-    source = "./modules/key_vault.tf"
+    source = "./module/key_vault.tf"
     allowed_subnet_ids      = $(ALLOWED_SUBNET_IDS)
     resource_group_name     = $(RESOURCE_GROUP_NAME)
     resource_group_location = $(RESOURCE_GROUP_LOCATION)
